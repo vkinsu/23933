7f53cb95329e29a62a280fe10d5665c611507f4d
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-19 17:30:44 +0700

    task2

---
+++ d.karpachev/task2/ex_time.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
+
+typedef void def;
+
+def main() {
+
+	struct tm* my_tm;
+	time_t now = time(0);
+	now -= 25200;
+	my_tm = gmtime(&now);
+
+	printf("California> %d/%d/%d %d:%d:%d\n", my_tm->tm_mday, my_tm->tm_mon + 1, my_tm->tm_year + 1900, 
+								my_tm->tm_hour, my_tm->tm_min, my_tm->tm_sec);
+
+	exit(0);
+}
