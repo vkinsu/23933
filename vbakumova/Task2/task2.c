97e41d74e7586559cbd42ce4ca311847bc3f42a8
M1roQ <b_val4@mail.ru>
2024-09-19 16:42:49 +0700

    edit task2, add makefile

--- vbakumova/Task2/task2.c
+++ vbakumova/Task2/task2.c
@@ -1,6 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
 
 int main() {
-	printf("Hello Lera!\n");
-	return 0;
+	time_t now;
+	struct tm* sp;
+
+	(void)time(&now);
+
+	now -= 25200;
+	sp = gmtime(&now);
+	printf("California:  %d/%d/%d  %d:%d:%d\n", sp->tm_mday, sp->tm_mon + 1, sp->tm_year + 1900, sp->tm_hour, sp->tm_min, sp->tm_sec);
+	exit(0);
 }
