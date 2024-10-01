139619c500fa15b73e065185b96f556055131c60
Vladislav Polyanskii <v.polyanskii@g.nsu.ru>
2024-10-01 17:53:28 +0700

    Task_1

---
+++ OS/Task_2/main.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    time_t now;
+    struct tm *sp;
+    setenv("TZ", "PST8PDT", 1);
+    
+    time(&now);
+
+    printf("%s", ctime(&now));
+
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+            sp->tm_mon + 1, sp->tm_mday,
+            sp->tm_year, sp->tm_hour,
+            sp->tm_min, tzname[sp->tm_isdst]);
+    return 0;
+}
