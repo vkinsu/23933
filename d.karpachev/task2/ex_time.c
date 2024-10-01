31254b7ea318795c56422f39a9591005686da010
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-10-01 16:36:50 +0700

    ex_time.c updated

--- d.karpachev/task2/ex_time.c
+++ d.karpachev/task2/ex_time.c
@@ -4,17 +4,15 @@
 #include <sys/types.h>
 #include <time.h>
 
-typedef void def;
-
-def main() {
+int main() {
 
 	struct tm* my_tm;
 	time_t now = time(0);
 	now -= 25200;
 	my_tm = gmtime(&now);
 
-	printf("California> %d/%d/%d %d:%d:%d\n", my_tm->tm_mday, my_tm->tm_mon + 1, my_tm->tm_year + 1900, 
+	printf("California> %d/%d/%d %d:%d:%d\n", my_tm->tm_mday, my_tm->tm_mon + 1, my_tm->tm_year + 1900,
 								my_tm->tm_hour, my_tm->tm_min, my_tm->tm_sec);
 
-	exit(0);
+	return 0;
 }
