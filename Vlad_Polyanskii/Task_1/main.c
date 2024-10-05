bc5f418a71e7f07f8608f432e9c7afaa23807918
Vladislav Polyanskii <v.polyanskii@g.nsu.ru>
2024-10-05 13:40:29 +0700

    Task

--- Vlad_Polyanskii/Task_1/main.c
+++ Vlad_Polyanskii/Task_1/main.c
@@ -1,24 +1,18 @@
-#include <unistd.h>
+#include <sys/time.h>
 #include <sys/resource.h>
+#include <unistd.h>
+#include<stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
 extern char *optarg;
-
 extern int optind, opterr, optopt;
-
 extern char **environ;
 
 int main(int argc, char** argv){
-    if(argc == 1){
-        printf("There are no arguments.\n");
-        return 0;
-    }
-
     char* opts = "ispuU:cC:dvV:";
-    int opt;
-    while((opt = getopt(argc, argv, opts)) != -1){
+    char opt;
+    while ((opt = getopt(argc, argv, opts)) != -1){
         switch (opt){
             case 'i':{
                 printf("euid: %u\tuid: %u\n", geteuid(), getuid());
@@ -26,7 +20,7 @@ int main(int argc, char** argv){
                 break;
             }
             case 's':{
-                printf("%d\n", setpgid(0, getpgid(0)));
+                printf("%d", setpgid(0, getpgid(0)));
                 break;
             }
             case 'p':{
@@ -34,49 +28,43 @@ int main(int argc, char** argv){
                 break;
             }
             case 'u':{
-                struct rlimit flimit;
-                if(!getrlimit(RLIMIT_FSIZE, &flimit))
-                    printf("ulimit_cur: %lu\tulimit_max: %lu\n", flimit.rlim_cur, flimit.rlim_max);
-                else printf("-1\n");
+                struct rlimit fsize;
+                getrlimit(RLIMIT_FSIZE, &fsize);
+                printf("FSIZE_CUR: %lu\t FSIZE_MAX: %lu\n", fsize.rlim_cur, fsize.rlim_max);
                 break;
             }
             case 'U':{
-                int new_value = strtol(optarg, NULL, 10);
-                if(new_value < 0){
-                    printf("Error: Needs type rlim_t aka unsigned long\n");
-                    return -1;
+                int new_rlim = atol(optarg);
+                if(new_rlim < 0){
+                    printf("Error: needs rlim_t aka unsigned int\n");
+                    exit(1);
                 }
-                struct rlimit flimit;
-                flimit.rlim_cur = (rlim_t)new_value;
-                flimit.rlim_max = (rlim_t)new_value;
-                printf("%d\n", setrlimit(RLIMIT_FSIZE, &flimit));
-                /*printf("%d\n", getrlimit(RLIMIT_FSIZE, &ulimit));
-                printf("ulimit_cur: %lu\tulimit_max: %lu\n", ulimit.rlim_cur, ulimit.rlim_max);*/
+                struct rlimit fsize;
+                fsize.rlim_cur = (unsigned)new_rlim;
+                fsize.rlim_max = (unsigned)new_rlim;
+                printf("%d\n", setrlimit(RLIMIT_FSIZE, &fsize));
                 break;
             }
             case 'c':{
-                struct rlimit core_file;
-                if(!getrlimit(RLIMIT_DATA, &core_file))
-                    printf("ulimit_cur: %lu\tulimit_max: %lu\n", core_file.rlim_cur, core_file.rlim_max);
-                else printf("-1\n");
+                struct rlimit cfsize;
+                getrlimit(RLIMIT_DATA, &cfsize);
+                printf("DATA_CUR: %lu\t DATA_MAX: %lu\n", cfsize.rlim_cur, cfsize.rlim_max);
                 break;
             }
             case 'C':{
-                int new_value = strtol(optarg, NULL, 10);
-                if(new_value < 0){
-                    printf("Error: Needs type rlim_t aka unsigned long\n");
-                    return -1;
+                int new_rlim = atol(optarg);
+                if(new_rlim < 0){
+                    printf("Error: needs rlim_t aka unsigned int\n");
+                    exit(1);
                 }
-                struct rlimit core_file;
-                core_file.rlim_cur = (rlim_t)new_value;
-                core_file.rlim_max = (rlim_t)new_value;
-                printf("%d\n", setrlimit(RLIMIT_DATA, &core_file));
-                /*printf("%d\n", getrlimit(RLIMIT_DATA, &core_file));
-                printf("core_file_cur: %lu\tcore_file_max: %lu\n", core_file.rlim_cur, core_file.rlim_max);*/
+                struct rlimit cfsize;
+                cfsize.rlim_cur = (unsigned)new_rlim;
+                cfsize.rlim_max = (unsigned)new_rlim;
+                printf("%d\n", setrlimit(RLIMIT_DATA, &cfsize));
                 break;
             }
             case 'd':{
-                printf("curr_dir: %s\n", getenv("PWD"));
+                printf("%s\n", getenv("PWD"));
                 break;
             }
             case 'v':{
@@ -87,25 +75,23 @@ int main(int argc, char** argv){
             }
             case 'V':{
                 char* name = NULL;
-                char* value = NULL;
+                char* value = (char*)malloc(sizeof(char));
+                value[0] = '\0';
 
                 int i;
-                for(i = 0; optarg[i] != '='; i++);
+                for(i = 0; optarg[i] != '=' && optarg[i] != '\0'; i++);
                 name = (char*)malloc(sizeof(char) * (i + 1));
                 memcpy(name, optarg, sizeof(char) * i);
-                name[i++] = '\0';
+                name[i] = '\0';
 
-                int value_size = 0;
-                char *from = &optarg[i];
-                for(; optarg[i] != '\0'; i++, value_size++);
-                value = (char*)malloc(sizeof(char) * ++value_size);
-                memcpy(value, from, value_size);
-                value[value_size] = '\0';
+                if(optarg[i] != '\0'){
+                    value = &optarg[++i];
+                }
 
                 printf("%s=%s\n", name, value);
                 printf("%d\n", setenv(name, value, 1));
                 break;
-            }     
+            }
             default:{
                 break;
             }
