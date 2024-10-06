7b6e146373ecc78e264d24603c3516bbc5219941
Enderser <enderser4@gmail.com>
2024-10-06 12:51:37 +0700

    Ðthe final ex_time

--- e.sergeev/task2/ex_time.c
+++ e.sergeev/task2/ex_time.c
@@ -1,15 +1,15 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <sys/types.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
 int main() {
-    time_t now = time(0);
-    now -= 25200;
-    struct tm* sp;
-    sp = gmtime(&now);
-    printf("Day: %d\nMonth: %d\nYear: %d\n%d:%d:%d", sp->tm_mday, sp->tm_mon + 1, 1900 + sp->tm_year,
-        sp->tm_hour, sp->tm_min, sp->tm_sec);
+    time_t now;
+    struct tm* timeinfo;
+    setenv("TZ", "America/Los_Angeles", 1);
+    tzset();
+    time(&now);
+    timeinfo = localtime(&now);
+    printf("%s", asctime(timeinfo));
     return 0;
 }
