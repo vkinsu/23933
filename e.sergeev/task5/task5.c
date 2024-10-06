723fe5515958978286dada84b0cb5e43f8605cdb
Enderser <enderser4@gmail.com>
2024-10-06 14:00:30 +0700

    task 5 solution

---
+++ e.sergeev/task5/task5.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define BUFFER_SIZE 1024
+
+int main() {
+    int fd;
+    char filename[] = "text.txt";
+    scanf("%s", filename);
+    off_t *offsets = NULL;
+    size_t *lengths = NULL;
+    char buffer[BUFFER_SIZE];
+    int line_count = 0, i;
+    ssize_t bytes_read;
+
+    if ((fd = open(filename, O_RDONLY)) == -1) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    off_t current_offset = 0;
+    offsets = (off_t *)malloc(sizeof(off_t));
+    lengths = (size_t *)malloc(sizeof(size_t));
+
+    if (offsets == NULL || lengths == NULL) {
+        perror("Memory allocation error");
+        close(fd);
+        return 1;
+    }
+
+    offsets[0] = 0;
+    size_t current_length = 0;
+
+    while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0) {
+        for (i = 0; i < bytes_read; ++i) {
+            if (buffer[i] == '\n') {
+                line_count++;
+                current_length++;
+                
+                offsets = realloc(offsets, (line_count + 1) * sizeof(off_t));
+                lengths = realloc(lengths, line_count * sizeof(size_t));
+
+                if (offsets == NULL || lengths == NULL) {
+                    perror("Memory allocation error");
+                    close(fd);
+                    return 1;
+                }
+
+                lengths[line_count - 1] = current_length;
+                offsets[line_count] = current_offset + i + 1;
+                current_length = 0;
+            } 
+            else current_length++;
+        }
+        current_offset += bytes_read;
+    }
+
+    if (bytes_read == -1) {
+        perror("Error reading file");
+        close(fd);
+        free(offsets);
+        free(lengths);
+        return 1;
+    }
+
+    int line_number;
+    while (1) {
+        printf("Enter line number (0 to exit): ");
+        scanf("%d", &line_number);
+
+        if (line_number == 0) break;
+        else if (line_number > 0 && line_number <= line_count) {
+            lseek(fd, offsets[line_number - 1], SEEK_SET);
+            size_t len = lengths[line_number - 1];
+            char *line = (char *)malloc(len + 1);
+
+            if (line == NULL) {
+                perror("Memory allocation error");
+                break;
+            }
+
+            if (read(fd, line, len) != len) {
+                perror("Error reading line");
+                free(line);
+                break;
+            }
+
+            line[len] = '\0';
+            printf("Line %d: %s", line_number, line);
+            free(line);
+        } 
+        else printf("Invalid line number.\n");
+    }
+
+    close(fd);
+    free(offsets);
+    free(lengths);
+
+    return 0;
+}
+
