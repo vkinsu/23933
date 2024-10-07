2202b626fa8e04722ebbd62106317bc813183e58
M1roQ <b_val4@mail.ru>
2024-10-08 01:00:16 +0700

    update files

--- vbakumova/Task2/task2.c
+++ vbakumova/Task2/task2.c
@@ -4,14 +4,22 @@
 #include <sys/types.h>
 #include <time.h>
 
+extern char* tzname[];
+
 int main() {
-	time_t now;
-	struct tm* sp;
+    time_t now;
+    struct tm* sp;
 
-	(void)time(&now);
+    putenv("TZ=America/Los_Angeles");
 
-	now -= 25200;
-	sp = gmtime(&now);
-	printf("California:  %d/%d/%d  %d:%d:%d\n", sp->tm_mday, sp->tm_mon + 1, sp->tm_year + 1900, sp->tm_hour, sp->tm_min, sp->tm_sec);
-	exit(0);
+    now = time(NULL);
+    printf("%s", ctime(&now));
+
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year - 100, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+
+    return 0;
 }
