0ff2f26dc94b61f780da5dd0287920533200f2d4
Vladislav Polyanskii <v.polyanskii@g.nsu.ru>
2024-10-12 13:45:10 +0700

    Tasks 6

--- Vlad_Polyanskii/Tasks_5-7/main.c
+++ Vlad_Polyanskii/Tasks_5-7/main.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <signal.h>
+
+int fd;
+int flen = 0;
 
 typedef struct str_info{
     int str_len;
@@ -13,7 +16,6 @@ typedef struct str_info{
 
 typedef struct table{
     int fd;
-    int flen;
     int lines;
     str_info* matrix;
 } table;
@@ -38,7 +40,7 @@ void new_line(table* t){
 int count_indents(table* t){
     char sym;
     while(read(t->fd, &sym, sizeof(char)) != 0){
-        t->flen += 1;
+        flen += 1;
         if(sym == ' '){
             t->matrix[t->lines - 1].indent_len += 1;
         }
@@ -58,7 +60,7 @@ int count_indents(table* t){
 int count_len(table* t){
     char sym;
     while(read(t->fd, &sym, sizeof(char)) != 0){
-        t->flen += 1;
+        flen += 1;
         t->matrix[t->lines - 1].str_len += 1;
         if(sym == '\n'){
             return 1;
@@ -72,7 +74,6 @@ int count_len(table* t){
 table* get_strs_info(int fd){
     table* t = (table*)malloc(sizeof(table));
     t->lines = 0, t->matrix = NULL; 
-    t->fd = fd, t->flen = 0;
     new_line(t);
     while (1){
         if(count_indents(t) == 0){
@@ -95,17 +96,17 @@ void print_str(table* t, int line){
     }
     offset += t->matrix[line].indent_len;
 
-    lseek(t->fd, offset * sizeof(char), SEEK_SET);
+    lseek(fd, offset * sizeof(char), SEEK_SET);
 
     char *buff = (char*)malloc(sizeof(char));
     int len = t->matrix[line].str_len;
-    read(t->fd, buff, sizeof(char) * (len - 1));
+    read(fd, buff, sizeof(char) * (len - 1));
     printf("%s\n", buff);
 }
 
-void print_file(table* t){
+void print_file(int signum){
     char *buff = (char*)malloc(sizeof(char));
-    read(t->fd, buff, sizeof(char) * t->flen);
+    read(fd, buff, sizeof(char) * flen);
     printf("FILE:\n%s\n", buff);
 }
 
@@ -115,7 +116,7 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    int fd = open(argv[1], O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
     if(fd == -1){
         perror("\nERROR\nCannot open the file");
         return 1;
@@ -128,7 +129,8 @@ int main(int argc, char** argv){
     int line;
     while(1){
         printf("in: ");
-        alarm(5, print_file(strs_info));
+        signal(SIGALARM, print_file)
+        alarm(5);
         scanf("%d", &line);
         if(line == 0 || line < 0  || line > strs_info->lines){
             return 0;
