7d4ebf375aaf8f652e755b1d29514582f718964b
Тишкин Андрей Алексеевич <74547328+andrei1112111@users.noreply.github.com>
2024-11-05 16:56:57 +0700

    fix 18

--- a.tishkin1/lab18/main.c
+++ a.tishkin1/lab18/main.c
@@ -60,10 +60,9 @@ void printFileInfo(const char *path) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        perror("exists filename arguments\n");
-        return 1;
+        printFileInfo(".");
+        return 0;
     }
-
     for (int i = 1; i < argc; i++) {
         printFileInfo(argv[i]);
     }
