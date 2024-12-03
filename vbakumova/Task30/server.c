4f722e59fa7a26f48943f6d06fe6b1bf25492b8a
M1roQ <b_val4@mail.ru>
2024-12-03 21:28:01 +0700

    update files

--- vbakumova/Task30/server.c
+++ vbakumova/Task30/server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/un.h>
@@ -13,18 +14,19 @@ int main(){
     int server_fd, client_fd;
     struct sockaddr_un server_addr;
 
-    // settings for socket's addr
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path));
-
-    unlink(SOCKET_PATH);
-
     // ctreature socket
     if((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
         perror("Error: Socket creation failed.");
         exit(EXIT_FAILURE);
     }
+
+    unlink(SOCKET_PATH);
+
+    // settings for socket's addr
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path));
+   
     
     // binding the socket
     if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
@@ -34,7 +36,7 @@ int main(){
 
     // change rejim to "connecting"
     if (listen(server_fd, 5) == -1) {
-        perror("Error: listening failed.");
+        perror("Error: Listening failed.");
         exit(EXIT_FAILURE);
     }
 
@@ -42,19 +44,19 @@ int main(){
 
     // Getting  onnect from client
     if ((client_fd = accept(server_fd, NULL, NULL)) == -1) {
-        perror("Accepted");
+        perror("Error: Acepted error.");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     char buf[BUFFER_SIZE];
-    ssize_t bytes_read;
+    ssize_t  bytes_read;
 
     while ((bytes_read = read(client_fd, buf, sizeof(buf) -1)) > 0){
-        for (ssize_t i = 0; i < bytes_read; i++) {
+        for (int i = 0; i < bytes_read; i++) {
             buf[i] = toupper(buf[i]);
         }
-        printf("Received and converted: %c\n", buf);
+        fwrite(buf, bytes_read, 1, stdout);
     }
 
     // close connectong
