0318875f0af0c83d1bcbb3cfedd6708db0109126
Vladislav Polyanskii <v.polyanskii@g.nsu.ru>
2024-12-08 18:23:03 +0700

    Task 32

---
+++ Vlad_Polyanskii/Task_32/client.c
@@ -0,0 +1,124 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <aio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+typedef struct pthread_data{
+    int fd;
+    int sig;
+    char* message;
+    int len;
+} pthread_data;
+
+int fds[2];
+
+int make_connect(int fd, struct sockaddr_un *addr){
+    if(connect(fd, (struct sockaddr*)addr, sizeof(*addr)) == -1){
+        close(fd);
+        return 1;
+    }
+    return 0;
+}
+
+void close_fds(int n){
+    for(int i = 0; i < n; i++){
+        close(fds[i]);
+    }
+}
+
+int servers_done[2];
+
+void handler(int sig){
+    close(fds[sig - SIGRTMIN - 1]);
+    servers_done[sig - SIGRTMIN - 1] = 1;
+    signal(sig, SIG_IGN);
+}
+
+void* send_message(void* arg){
+    pthread_data* pd = (pthread_data*)arg;
+    struct aiocb writerq;
+    memset(&writerq, 0, sizeof(writerq));
+    writerq.aio_fildes = pd->fd;
+    writerq.aio_buf = pd->message;
+    writerq.aio_nbytes = pd->len;
+    writerq.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    writerq.aio_sigevent.sigev_signo = pd->sig;
+
+    signal(pd->sig + 1, handler);
+    aio_write(&writerq);
+    while(1){
+        int ret = aio_return(&writerq);
+        if(ret == -1){
+            if(errno != EINPROGRESS){
+                perror("aio write error");
+                return arg;
+            }
+        }
+        else break;  
+    }
+
+    while(servers_done[pd->sig - SIGRTMIN] != 1);
+}
+
+void init_sockets(int n){
+    for(int i = 0; i < n; i++){
+        fds[i] = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    }
+}
+
+int main(){
+    memset(servers_done, 0, sizeof(servers_done));
+    int n = 2;
+    init_sockets(n);
+
+    struct sockaddr_un addr;
+    char* socket_path = "socket";
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+
+    if(make_connect(fds[1], &addr) == 1){
+        perror("Connect error");
+        return 1;
+    }
+
+    if(make_connect(fds[0], &addr) == 1){
+        perror("Connect error");
+        return 1;
+    }
+
+    pthread_t tids[2];
+
+    pthread_data pd0;
+    pd0.fd = fds[0];
+    pd0.sig = SIGRTMIN;
+    pd0.message = "Hello world!";
+    pd0.len = strlen(pd0.message);
+
+    pthread_data pd1;
+    pd1.fd = fds[1];
+    pd1.sig = SIGRTMIN + 2;
+    pd1.message = "Another message";
+    pd1.len = strlen(pd1.message);
+
+    if(pthread_create(&tids[0], NULL, send_message, &pd0) != 0){
+        perror("Thread create error");
+        return 1;
+    }
+    if(pthread_create(&tids[1], NULL, send_message, &pd1) != 0){
+        perror("Thread create error");
+        return 1;
+    }
+    pthread_join(tids[0], NULL);
+    printf("Client 1 send a message\n");
+    pthread_join(tids[1], NULL);
+    printf("Client 2 send a message\n");
+    return 0;
+}
---
+++ Vlad_Polyanskii/Task_32/server.c
@@ -0,0 +1,152 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <aio.h>
+#include <pthread.h>
+#include <errno.h>
+#include <signal.h>
+
+typedef struct pthread_data{
+    int fd;
+    int client_id;
+} pthread_data;
+
+int end_messages[2];
+
+void sa_sigaction(int sig, siginfo_t* sig_inf, void* arg){
+    if(sig == SIGRTMIN){
+        end_messages[0] = 1;
+        sigaction(sig, SIG_IGN, NULL);
+    }
+    if(sig == SIGRTMIN + 2){
+        end_messages[1] = 1;
+        sigaction(sig, SIG_IGN, NULL);
+    }
+}
+
+void* get_message(void* arg){
+    pthread_data* pd = (pthread_data*)arg;
+    struct aiocb readrq;
+    memset(&readrq, 0, sizeof(readrq));
+    readrq.aio_fildes = pd->fd;
+    char sym;
+    readrq.aio_buf = &sym;
+    readrq.aio_nbytes = 1;
+
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    act.sa_flags |= SA_SIGINFO;
+    act.sa_sigaction = sa_sigaction;
+    sigaction(SIGRTMIN + pd->client_id, &act, NULL);
+    
+    int len = 0;
+    aio_read(&readrq);
+    while(1){
+        int ret = aio_return(&readrq);
+        switch (ret){
+            case 0:{
+                if(end_messages == 1 && len > 0){
+                    sigsend(P_ALL, 0, SIGRTMIN + pd->client_id + 1);
+                    close(pd->fd);
+                    return NULL;
+                }
+                break;
+            }
+            case -1:{
+                if(errno != EINPROGRESS){
+                    perror("aio read error");
+                    return arg;
+                }
+                break;
+            }
+            default:{
+                len++;
+                putchar(toupper(sym));
+                aio_read(&readrq);
+                break;
+            }
+        }
+    }
+
+}
+
+int main(int argc, char** argv){
+    int fd = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    if(fd == -1){
+        perror("Socket error");
+        return 1;
+    }
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    char* socket_path = "socket";
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+    unlink(socket_path);
+
+    if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == -1){
+        perror("Bind error");
+        close(fd);
+        return 1;
+    } 
+
+    if(listen(fd, 2) == -1){
+        perror("Listen error");
+        close(fd);
+        return 1;
+    }
+
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("Can't create child process");
+        close(fd);
+        return -1;
+    }
+
+    if(pid == 0){
+        if(execvp("./client", argv) == -1){
+            perror("execvp error");
+            return 1;
+        }
+        return 0;
+    }
+    else{
+        memset(end_messages, 0, sizeof(end_messages));
+        pthread_t tids[2];
+        pthread_data clients_data[2];
+
+        printf("\nServer output:\n");
+        int connects = 0;
+        int curr_id = 0;
+        while(connects < 2){
+            int client_fd = accept(fd, NULL, NULL);
+            if(client_fd == -1){
+                continue;
+            }
+
+            clients_data[connects].fd = client_fd;
+            clients_data[connects].client_id = curr_id;
+            curr_id += 2;
+
+            if(pthread_create(&tids[connects], NULL, get_message, &clients_data[connects]) != 0){
+                perror("Thread create error");
+                return 1;
+            }
+            printf("Server start reading a message %d\n", connects);
+            connects++;
+        }
+
+        pthread_join(tids[0], NULL);
+        printf("Server got a message %d\n", connects);
+        pthread_join(tids[1], NULL);
+        printf("Server got a message %d\n", connects);
+    }
+    close(fd);
+    return 0;
+}
